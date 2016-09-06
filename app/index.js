9dd371e5af5cb27aa3218500995eb43b9156774e
dj_pale <espen.breivik@gmail.com>
2016-09-07 00:19:42 +0200

    * Filters are working! * Category counts are working!

--- app/index.js
+++ app/index.js
@@ -1,23 +1,109 @@
 angular.module('grabtangle', ['ngAnimate', 'ui.bootstrap', 'datetime']);
 angular.module('grabtangle').controller('AccordionDemoCtrl', function ($scope, $window) 
 {
+  const DAY_ADD = 86400000;
+
+  generateDates();
+
   $scope.oneAtATime = true;
 
   $scope.tasks = 
   [
-    { completed: false, project: 'Grabtangle', action: 'Test databinding', category: '1', due: new Date('2016-08-17'), waiting: false, isOpen: false },
+    { completed: false, project: 'Grabtangle', action: 'Test databinding', category: '1', due: new Date('2016-09-06'), waiting: false, isOpen: false },
     { completed: false, project: 'Raspberry PI', action: 'Check network boot stuff (@NoCode)', category: '1', due: new Date('2016-08-25'), waiting: false, isOpen: false }
   ];
 
-  const DAY_ADD = 86400000;
+  $scope.test = [];
+
+  $scope.filterCount = { 'Today': 0, 'Week': 0, 'Waiting': 0, 'All': 0 };
+
+  $scope.activeFilter = 'Today';
+  $scope.filter = [];
+  $scope.filter['Today'] = function(item)
+  {
+    return (item.completed == false && item.waiting == false && item.due < $scope.dates[1].d);
+  };
+
+  $scope.filter['Week'] = function(item)
+  {
+    return (item.completed == false && item.waiting == false && item.due < $scope.dates[3].d);
+  };
+
+  $scope.filter['Waiting'] = function(item)
+  {
+    return (item.completed == false && item.waiting == true);
+  };
+  
+  $scope.filter['All'] = function(item)
+  {
+    return (item.completed == false);
+  };
+
+  $scope.filterCategory = function()
+  {
+    return $scope.filter[$scope.activeFilter];
+  };
+
+  refreshCount();
 
   var undo_obj = null;
+
+  function generateDates()
+  {
+    let today = new Date();
+    today.setHours(0);
+    today.setMinutes(0);
+    today.setSeconds(0);
+    today.setMilliseconds(0);
+
+    let tomorrow = new Date(today.valueOf() + DAY_ADD);
+
+    let dow = today.getDay();
+
+    let weekend = new Date(today.valueOf());
+    if (dow > 0 && dow < 6)
+    {
+      weekend.setTime(weekend.valueOf() + ((6 - dow) * DAY_ADD));
+    }
+
+    let nextweek = new Date(today.valueOf());
+    let daystonext = (7 - dow + 1) % 7; // how long to next day of week (Monday)
+    if (daystonext == 0) daystonext = 7; // this means next Monday if we are already on Monday
+    nextweek.setTime(nextweek.valueOf() + daystonext * DAY_ADD);
+    
+    let twoweeks = new Date(today.valueOf());
+    twoweeks.setTime(twoweeks.valueOf() + 14 * DAY_ADD); 
+
+    $scope.dates = [];
+    $scope.dates.push({ n: 'Today', d: today});
+    $scope.dates.push({ n: 'Tomorrow', d: tomorrow});
+    $scope.dates.push({ n: 'Weekend', d: weekend});
+    $scope.dates.push({ n: 'Next week', d: nextweek});
+    $scope.dates.push({ n: '2 weeks', d: twoweeks});
+  }
+
+
+  function refreshCount()
+  {
+    angular.forEach(Object.keys($scope.filter), function(key)
+    {
+      $scope.filterCount[key] = 0;
+    });
+
+    angular.forEach($scope.tasks, function(item)
+    {
+      angular.forEach(Object.keys($scope.filter), function(key)
+      {
+        if ($scope.filter[key](item)) $scope.filterCount[key]++;
+      });
+    });
+  }
 
   function setUndo(task)
   {
     if (task)
     {
-      undo_obj = { completed: task.completed, project: task.project, action: task.action, category: task.category, due: new Date(task.valueOf()), waiting: task.waiting, isOpen: task.isOpen}
+      undo_obj = { completed: task.completed, project: task.project, action: task.action, category: task.category, due: new Date(task.valueOf()), waiting: task.waiting, isOpen: task.isOpen};
     }
   }
 
@@ -29,42 +115,23 @@ angular.module('grabtangle').controller('AccordionDemoCtrl', function ($scope, $
       //undo_obj = null;
     }
   }
-
-  let today = new Date();
-  today.setHours(0);
-  today.setMinutes(0);
-  today.setSeconds(0);
-  today.setMilliseconds(0);
-
-  let tomorrow = new Date(today.valueOf() + DAY_ADD);
-
-  let dow = today.getDay();
-
-  let weekend = new Date(today.valueOf());
-  if (dow > 0 && dow < 6)
-  {
-    weekend.setTime(weekend.valueOf() + ((6 - dow) * DAY_ADD));
-  }
-
-  //let weekend = 
-  //let nextweek =
-  //let endweek =
-  //let twoweeks = 
-  //let endmonth =
-
-  $scope.dates = [];
-  $scope.dates.push({ n: 'Today', d: today});
-  $scope.dates.push({ n: 'Tomorrow', d: tomorrow});
-  $scope.dates.push({ n: 'Weekend', d: weekend});
   
   $scope.setNewDate = function($event,task,d)
   {
-    task.due = d.d;
+    if (task)
+    {
+      task.due = d.d;
+      refreshCount();
+    }
   };
 
   $scope.completeTask = function($event,task)
   {
-    if (task) task.completed = true;
+    if (task)
+    { 
+      task.completed = true;
+      refreshCount();
+    }
     $event.stopPropagation();
   };
 
@@ -75,19 +142,19 @@ angular.module('grabtangle').controller('AccordionDemoCtrl', function ($scope, $
       task.isOpen = true;
       setUndo(task);
       task.action = '';
+      refreshCount();
     }
     $event.stopPropagation();
   };
 
-  $scope.waiting = function($event,task)
+  $scope.waitTask = function($event,task)
   {
-    setUndo(task);
-    task.waiting = true;
-    $event.stopPropagation();
-  }
-
-  $scope.testClick = function($event,task)
-  {
+    if (task)
+    {
+      setUndo(task);
+      task.waiting = !task.waiting;
+      refreshCount();
+    }
     $event.stopPropagation();
   };
 })
@@ -108,7 +175,7 @@ angular.module('grabtangle').controller('AccordionDemoCtrl', function ($scope, $
         link: function (scope, element, attrs) {
             attrs.$observe('autoFocus', function(newValue){
                 if (newValue === 'true')
-                    $timeout(function(){element[0].focus()});
+                    $timeout(function(){element[0].focus();});
             });
         }
     };
