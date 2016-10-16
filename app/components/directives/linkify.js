0f261fee232821e5ba42c841f7af35b96da7ef84
dj_pale <espen.breivik@gmail.com>
2016-10-16 20:17:20 +0200

    Support multiple hashtags https://github.com/DjPale/Grabtangle/issues/8

--- app/components/directives/linkify.js
+++ app/components/directives/linkify.js
@@ -2,31 +2,54 @@ angular.module('grabtangle').directive('linkify', ['$compile', function($compile
 {
     function processValue(value, scope, element, attrs)
     {
-        var txt = value;
+        let txt = value;
 
         if (attrs.keyword == '#')
         {
-            element.html(txt);
-            // TODO: loop for each # found to create multiple links
-            var pos = txt.indexOf('#');
+            // create a separate target txt to avoid messing with our source string
+            let dstTxt = '';
+            let pos = txt.indexOf('#');
+            let startIdx = 0;
 
-            if (pos == -1 || pos >= txt.length - 1) return;
+            while (pos != -1 && pos < txt.length - 1)
+            {
+                let spacePos = txt.indexOf(' ', pos + 1);
+                // if no space, it means that the tag spans the rest of the string
+                if (spacePos == -1) spacePos = txt.length;
 
-            var spacePos = txt.indexOf(' ', pos + 1);
-            if (spacePos == -1) spacePos = txt.length;
+                // must have at least one character in hash tag
+                if (spacePos - pos > 1)
+                {
+                    let keyword = txt.substring(pos, spacePos);
 
-            if (spacePos - pos <= 1) return;
+                    dstTxt += txt.substring(startIdx, pos);
+                    dstTxt += '<a style="cursor:pointer;" ng-click="' + attrs.handler.replace('#', keyword) + '">' + keyword + '</a>';
+                }
 
-            var keyword = txt.substring(pos, spacePos);
+                startIdx = spacePos;
 
-            txt = txt.replace(keyword, '<a style="cursor:pointer;" ng-click="' + attrs.handler.replace('#', keyword) + '">' + keyword + '</a>');
+                pos = txt.indexOf('#', startIdx);
+            }
+
+            // if we have filled in our dst text variable we must have made at least one substitution
+            if (dstTxt.length > 0) 
+            {
+                // we could have a leftover string after the last substitution
+                if (startIdx < txt.length) dstTxt += txt.substring(startIdx);
+                element.html(dstTxt);
+            }
+            else
+            {
+                element.html(txt);
+                return;
+            }
         }
         else
         {
             txt = txt.replace(attrs.keyword, '<a style="cursor:pointer;" ng-click="' + attrs.handler + '">' + attrs.keyword + '</a>');
+            element.html(txt);
         }
 
-        element.html(txt);
         $compile(element.contents())(scope);
     }
 
